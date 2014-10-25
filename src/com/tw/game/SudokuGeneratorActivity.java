4840f49983ee59b9d983e5961fa6dc157e2d4897
Kashishm <kashishmunjal64@gmail.com>
2014-10-25 12:47:37 +0530

    [#1005][Kashish,Shweta] showing result/errors after click on done button

--- src/com/tw/game/SudokuGeneratorActivity.java
+++ src/com/tw/game/SudokuGeneratorActivity.java
@@ -1,7 +1,10 @@
 package com.tw.game;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -11,7 +14,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.tw.game.factory.SudokuFactory;
+import com.tw.game.result.Error;
+import com.tw.game.result.Result;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,8 +47,6 @@ public class SudokuGeneratorActivity extends Activity {
         sudokuGrid.add(Arrays.asList(R.id.r8_c0, R.id.r8_c1, R.id.r8_c2, R.id.r8_c3, R.id.r8_c4, R.id.r8_c5, R.id.r8_c6, R.id.r8_c7, R.id.r8_c8));
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                System.out.println();
-
                 EditText number = (EditText) findViewById(sudokuGrid.get(i).get(j));
 
                 if (sudokuPuzzle.get(i).get(j) != null) {
@@ -66,28 +70,70 @@ public class SudokuGeneratorActivity extends Activity {
         }
     }
 
-    public void editfield(View view) {
+    public void editField(View view) {
         if (selectedTextView == null) return;
         selectedTextView.setText(((Button) view).getText());
     }
 
     public void solvePuzzle(View view) {
         ArrayList<ArrayList<Integer>> solvedPuzzle = sudoku.getSolvedPuzzle();
-
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 EditText number = (EditText) findViewById(sudokuGrid.get(i).get(j));
-
                 number.setText(String.valueOf(solvedPuzzle.get(i).get(j)));
                 number.setTextColor(Color.parseColor("#2709E6"));
                 if (sudokuPuzzle.get(i).get(j) != null) {
-                    number.setFocusable(false);
+                    number.setText(String.valueOf(solvedPuzzle.get(i).get(j)));
                     number.setTypeface(null, Typeface.BOLD_ITALIC);
+                    number.setFocusable(false);
                     number.setTextColor(Color.BLACK);
                 }
             }
         }
-
     }
 
+    public void showResult(View view) {
+        List<List<Integer>> userSolution = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            userSolution.add(new ArrayList<Integer>());
+            for (int j = 0; j < 9; j++) {
+                TextView textView = (TextView) findViewById(sudokuGrid.get(i).get(j));
+                userSolution.get(i).add(Integer.parseInt(String.valueOf(textView.getText())));
+            }
+        }
+        Result result = this.sudoku.validateSolution(userSolution);
+        if (result.isCorrect()) {
+            Intent yesAction = new Intent(SudokuGeneratorActivity.this, SudokuGeneratorActivity.class);
+            alertMessageBuilder("Congratulations! You won. Do you want to start a new game?", yesAction, 0);
+        } else {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    EditText number = (EditText) findViewById(sudokuGrid.get(i).get(j));
+                    if(number.getCurrentTextColor() == getResources().getColor(R.color.error_background))
+                        ((EditText) findViewById(sudokuGrid.get(i).get(j))).setTextColor(Color.parseColor("#2709E6"));
+                }
+            }
+            for (Error error : result.getErrors())
+                ((EditText) findViewById(sudokuGrid.get(error.getRow()).get(error.getColumn()))).setTextColor(getResources().getColor(R.color.error_background));
+            Toast.makeText(this, "Your Solution is not right.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void alertMessageBuilder(String message, final Intent yesAction, final int status) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(yesAction);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
