cc87577e1d7884e9a6cbfffb20678a2231f2b8b7
Shweta <shwetadongare28@gmail.com>
2014-10-30 13:28:50 +0530

    [#1014] [Kashish, Shweta] added numbers for input for solver

--- src/com/tw/game/SudokuSolverActivity.java
+++ src/com/tw/game/SudokuSolverActivity.java
@@ -1,10 +1,15 @@
 package com.tw.game;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import com.tw.game.factory.SudokuFactory;
 import com.tw.game.level.ThreeDifficultyLevels;
 
@@ -13,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SudokuSolverActivity extends Activity {
+    private TextView selectedTextView;
     private List<List<Integer>> sudokuGrid = new ArrayList<>();
     private List<List<Integer>> solvedPuzzle = new ArrayList<>();
     private List<List<Integer>> puzzle = new ArrayList<>();
@@ -24,7 +30,7 @@ public class SudokuSolverActivity extends Activity {
         SudokuActivity.addTextViews(this.sudokuGrid);
     }
 
-    public void solveUserPuzzle(View view) {
+    public void showResult(View view) {
         for (int i = 0; i < 9; i++) {
             puzzle.add(Arrays.asList(new Integer[9]));
             for (int j = 0; j < 9; j++) {
@@ -43,12 +49,27 @@ public class SudokuSolverActivity extends Activity {
             for (int j = 0; j < 9; j++) {
                 EditText number = (EditText) findViewById(sudokuGrid.get(i).get(j));
                 number.setText("");
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(number.getWindowToken(), 0);
+                number.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        selectedTextView = (TextView) view;
+                        return false;
+                    }
+                });
             }
+
     }
 
     public void loadPuzzle(View view) {
         finish();
         startActivity(new Intent(this, SudokuGeneratorActivity.class));
+    }
+
+    public void editField(View view) {
+        if (selectedTextView == null) return;
+        selectedTextView.setText(((Button) view).getText());
     }
 
     private void showSolvePuzzle(ArrayList<ArrayList<Integer>> solvedPuzzle) {
