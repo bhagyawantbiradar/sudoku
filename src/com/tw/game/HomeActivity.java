c8e530a7b0282537c4b261c562ac97471bbd6a36
surajbab <surajbab@INsurajbab.local>
2014-11-06 10:23:01 +0530

    [#1024 - Kashish/Samiksha - added homescreen with links to solver and generator.

---
+++ src/com/tw/game/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.tw.game;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.tw.game.alert.AlertDialogRadio;
+import com.tw.game.alert.AlertPositiveListener;
+
+public class HomeActivity extends Activity implements AlertPositiveListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.homeview);
+    }
+
+    public void loadSolver(View view) {
+        Intent intent = new Intent(this, SudokuSolverActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    public void onLevelSelection(View v) {
+        FragmentManager manager = getFragmentManager();
+        AlertDialogRadio alert = new AlertDialogRadio();
+        alert.setAlertPositiveListener(this);
+        Bundle b = new Bundle();
+        b.putInt("position", 0);
+        alert.setArguments(b);
+        alert.show(manager, "alert_dialog_radio");
+    }
+
+    @Override
+    public void onPositiveClick(String level) {
+        Intent intent = new Intent(this, SudokuGeneratorActivity.class);
+        intent.putExtra("level", level);
+        finish();
+        startActivity(intent);
+    }
+}
